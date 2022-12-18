ee17630a3607738ce36c17771a3717da26139997
Lenoxx in Box <lenoxxinbox@yandex.ru>
2022-12-18 12:19:00 +0300

    Initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,99 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestClass {
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.startMaximized = true;
+        open("http://localhost:9999");
+    }
+
+        @Test
+        @DisplayName("Should successful plan meeting")
+        void shouldSuccessfulPlanMeeting() {
+            var validUser = DataGenerator.Registration.generateUser("ru");
+            var daysToAddForFirstMeeting = 4;
+            var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+            var daysToAddForSecondMeeting = 7;
+            var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+            $("[data-test-id=city] input").setValue(validUser.getCity());
+            $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
+            $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+            $("[data-test-id=date] input").setValue(firstMeetingDate);
+            $("[data-test-id=name] input").setValue(validUser.getName());
+            $("[data-test-id=phone] input").setValue(validUser.getPhone());
+            $("[data-test-id=agreement]").click();
+            $(By.className("button")).click();
+
+            $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        }
+
+    @Test
+    @DisplayName("Should successful plan and notification about reschedule meeting")
+    void shouldSuccessfulPlanAndNotificationAboutRescheduleMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        $("[data-test-id=city] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=city] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $("[data-test-id=name] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=name] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=phone] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $(By.className("button")).click();
+
+        $("[data-test-id=replan-notification]").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("Should successful plan and reschedule meeting")
+    void shouldSuccessfulPlanAndRescheduleMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $(By.className("button")).click();
+        $x("//span[contains(text(), 'Перепланировать')]").click();
+
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
+}
